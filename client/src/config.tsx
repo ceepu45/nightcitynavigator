ae7ca32d088d86a4001bc81d0aa2e635ba160313
Evan Wildenberg <ewilden2017@gmail.com>
2025-03-29 18:49:16 -0600

    Switch to new server URL

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -1,10 +1,10 @@
 
 // URL template for the tile server
-export const TILE_URL = "https://maps.cpusocket.net/tiles/nightcity/{z}/{x}/{y}.png";
+export const TILE_URL = "https://tile.nightcitynavigator.com/{z}/{x}/{y}.png";
 
 // Base URL for nominatim
-export const NOMINATIM_URL = "https://maps.cpusocket.net/nominatim"
+export const NOMINATIM_URL = "https://nominatim.nightcitynavigator.com";
 
 // Base URL for valhalla
-export const VALHALLA_URL = "https://maps.cpusocket.net/valhalla/"
+export const VALHALLA_URL = "https://valhalla.nightcitynavigator.com";
 
